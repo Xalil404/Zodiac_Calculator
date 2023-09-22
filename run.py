bd7966fe2e8d7dd6202f567e964cb99116c18196
TEST <test@TESTs-MacBook-Pro.local>
2023-09-22 18:04:13 +0100

    Added step 2 Mayan calendar logic

--- run.py
+++ run.py
@@ -14,7 +14,9 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('zodiac_generator')
 
-wks = SHEET.worksheet('western')
+wks_western = SHEET.worksheet('western')
+wks_mayan = SHEET.worksheet('mayan')
+wks_chinese = SHEET.worksheet('chinese')
 
 print (
     """
@@ -47,34 +49,61 @@ m = input("Enter the Month ::>")
 
 def zodiac_sign(day, month):
    if month == '12':
-      astro_sign = 'Sagittarius' if (day < 22) else 'capricorn'
+      astro_sign = 'Sagittarius' if (day < 22) else 'Capricorn'
    elif month == '1':
-      astro_sign = 'Capricorn' if (day < 20) else 'aquarius'
+      astro_sign = 'Capricorn' if (day < 20) else 'Aquarius'
    elif month == '2':
-      astro_sign = 'Aquarius' if (day < 19) else 'pisces'
+      astro_sign = 'Aquarius' if (day < 19) else 'Pisces'
    elif month == '3':
-      astro_sign = 'Pisces' if (day < 21) else 'aries'
+      astro_sign = 'Pisces' if (day < 21) else 'Aries'
    elif month == '4':
-      astro_sign = 'Aries' if (day < 20) else 'taurus'
+      astro_sign = 'Aries' if (day < 20) else 'Taurus'
    elif month == '5':
-      astro_sign = 'Taurus' if (day < 21) else 'gemini'
+      astro_sign = 'Taurus' if (day < 21) else 'Gemini'
    elif month == '6':
-      astro_sign = 'Gemini' if (day < 21) else 'cancer'
+      astro_sign = 'Gemini' if (day < 21) else 'Cancer'
    elif month == '7':
-      astro_sign = 'Cancer' if (day < 23) else 'leo'
+      astro_sign = 'Cancer' if (day < 23) else 'Leo'
    elif month == '8':
-      astro_sign = 'Leo' if (day < 23) else 'virgo'
+      astro_sign = 'Leo' if (day < 23) else 'Virgo'
    elif month == '9':
-      astro_sign = 'Virgo' if (day < 23) else 'libra'
+      astro_sign = 'Virgo' if (day < 23) else 'Libra'
    elif month == '10':
-      astro_sign = 'Libra' if (day < 23) else 'scorpio'
+      astro_sign = 'Libra' if (day < 23) else 'Scorpio'
    elif month == '11':
-      astro_sign = 'scorpio' if (day < 22) else 'sagittarius'
+      astro_sign = 'Scorpio' if (day < 22) else 'Sagittarius'
    else:
         print("Only Months 1 through 12 & days 1 through 31 are accepted")
         return None
    return astro_sign
 
+def zodiac_sign_mayan(day, month):
+    if month == '12':
+      astro_sign = 'ALLIGATOR' if (day < 22) else 'FALCON'
+    elif month == '1':
+        astro_sign = 'FALCON' if (day < 20) else 'JAGUAR'
+    elif month == '2':
+        astro_sign = 'JAGUAR' if (day < 19) else 'FOX'
+    elif month == '3':
+        astro_sign = 'FOX' if (day < 21) else 'SNAKE'
+    elif month == '4':
+        astro_sign = 'SNAKE' if (day < 20) else 'SQUIRREL'
+    elif month == '5':
+        astro_sign = 'SQUIRREL' if (day < 21) else 'TURTLE'
+    elif month == '6':
+        astro_sign = 'TURTLE' if (day < 21) else 'BAT'
+    elif month == '7':
+        astro_sign = 'BAT' if (day < 23) else 'SCORPION'
+    elif month == '8':
+        astro_sign = 'SCORPION' if (day < 23) else 'DEER'
+    elif month == '9':
+        astro_sign = 'DEER' if (day < 23) else 'OWL'
+    elif month == '10':
+        astro_sign = 'OWL' if (day < 23) else 'PEACOCK'
+    elif month == '11':
+        astro_sign = 'PEACOCK' if (day < 22) else 'ALLIGATOR'
+    
+    return astro_sign
 
 
 
@@ -82,23 +111,48 @@ def zodiac_sign(day, month):
 #print(wks.row_values(1))
 #print(wks.col_values(1))
 
-df = pd.DataFrame({"Zodiac Sign": wks.row_values(1), "Qualities": wks.row_values(2), "Horoscope": wks.row_values(3)})
+df_western = pd.DataFrame({"Zodiac Sign": wks_western.row_values(1), "Qualities": wks_western.row_values(2), "Horoscope": wks_western.row_values(3)})
+
+df_mayan = pd.DataFrame({"Zodiac Sign": wks_mayan.row_values(1), "Qualities": wks_mayan.row_values(2), "Horoscope": wks_mayan.row_values(3)})
+
+df_chinese = pd.DataFrame({"Zodiac Sign": wks_chinese.row_values(1), "Qualities": wks_chinese.row_values(2), "Horoscope": wks_chinese.row_values(3)})
 
 
 zodiac = zodiac_sign(d, m)
+
 
 if zodiac:
     #xprint(zodiac)
 
     ### Pandas Filtering
-    zodiac_df = df[df["Zodiac Sign"] == zodiac]
-    qualities = zodiac_df["Qualities"].values[0]
-    horoscope = zodiac_df["Horoscope"].values[0]
+    zodiac_df_western = df_western[df_western["Zodiac Sign"] == zodiac]
+    qualities_western = zodiac_df_western["Qualities"].values[0]
+    horoscope_western = zodiac_df_western["Horoscope"].values[0]
     #print(qualities)
 
     #print()
     #print(horoscope)
 
-    print(f"Hey {Name}, your zodiac sign is {zodiac}. Your qualties are {qualities}. Your horoscope is:\n{horoscope}")
+    print(f"Hey {Name}, your zodiac sign is {zodiac}. Your qualties are {qualities_western}. Your horoscope is:\n{horoscope_western}")
+
+    mayan_choice = input("Would you like to receive your Mayan Zodiac sign & horoscope? (y/n)").lower()
+
+    if mayan_choice == 'y' or mayan_choice == 'yes':
+        # mayan logic
+        zodiac_mayan = zodiac_sign_mayan(d, m)
+
+        zodiac_df_mayan = df_mayan[df_mayan["Zodiac Sign"] == zodiac_mayan]
+        qualities_mayan = zodiac_df_mayan["Qualities"].values[0]
+        horoscope_mayan = zodiac_df_mayan["Horoscope"].values[0]
+
+        print(f"{Name}, your Mayan zodiac sign is {zodiac_mayan}. Your qualties are {qualities_mayan}. Your Mayan horoscope is:\n{horoscope_mayan}")
+
+
+
+
+    else:
+        # restart the program
+        pass
+
 
 
