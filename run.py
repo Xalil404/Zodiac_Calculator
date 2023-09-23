90684516a2ba2a83b1b4ab1873d981211e3fedc9
TEST <test@TESTs-MacBook-Pro.local>
2023-09-23 11:11:40 +0100

    Added final command to restart the program and loop for restarting the program

--- run.py
+++ run.py
@@ -2,7 +2,6 @@ import gspread
 from google.oauth2.service_account import Credentials
 import pandas as pd
 
-
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
@@ -17,36 +16,6 @@ SHEET = GSPREAD_CLIENT.open('zodiac_generator')
 wks_western = SHEET.worksheet('western')
 wks_mayan = SHEET.worksheet('mayan')
 wks_chinese = SHEET.worksheet('chinese')
-
-print (
-    """
-
-                   ________    ______    _______   __       ___       ______                               
-                  |       /   /  __  \  |       \ |  |     /   \     /      |                              
-                  `---/  /   |  |  |  | |  .--.  ||  |    /  ^  \   |  ,----'                              
-                     /  /    |  |  |  | |  |  |  ||  |   /  /_\  \  |  |                                   
-                    /  /----.|  `--'  | |  '--'  ||  |  /  _____  \ |  `----.                              
-                   /________| \______/  |_______/ |__| /__/     \__\ \______|                              
-                                                                                                           
-  ______     ___       __        ______  __    __   __          ___   .___________.  ______   .______      
- /      |   /   \     |  |      /      ||  |  |  | |  |        /   \  |           | /  __  \  |   _  \     
-|  ,----'  /  ^  \    |  |     |  ,----'|  |  |  | |  |       /  ^  \ `---|  |----`|  |  |  | |  |_)  |    
-|  |      /  /_\  \   |  |     |  |     |  |  |  | |  |      /  /_\  \    |  |     |  |  |  | |      /     
-|  `----./  _____  \  |  `----.|  `----.|  `--'  | |  `----./  _____  \   |  |     |  `--'  | |  |\  \----.
- \______/__/     \__\ |_______| \______| \______/  |_______/__/     \__\  |__|      \______/  | _| `._____|
-                                                                                                           
-
-    """
-)
-
-
-Name=input("Enter Your Name:")
-print ("Hello",str(Name))
-print("This Program Will determine Your Horoscope & Zodiac Signs in the Gregorian, Mayan and Chinese calendars.")
-
-d = int(input("Enter Day (1 through 31) ::>"))
-m = int(input("Enter the Month (1 through 12) ::>"))
-y = int(input("Enter the Year (From 1924 & onwards) ::>"))
 
 def zodiac_sign(day, month):
    if month == 12:
@@ -117,81 +86,120 @@ def zodiac_sign_chinese(y):
     else:
         zodiac_num = (diff % 12)
         return zodiac_num
-
-#print(wks.cell(1, 1))
-#print(wks.row_values(1))
-#print(wks.col_values(1))
-
-df_western = pd.DataFrame({"Zodiac Sign": wks_western.row_values(1), "Qualities": wks_western.row_values(2), "Horoscope": wks_western.row_values(3)})
-
-df_mayan = pd.DataFrame({"Zodiac Sign": wks_mayan.row_values(1), "Qualities": wks_mayan.row_values(2), "Horoscope": wks_mayan.row_values(3)})
-
-df_chinese = pd.DataFrame({"Zodiac Sign": wks_chinese.row_values(1), "Qualities": wks_chinese.row_values(2), "Horoscope": wks_chinese.row_values(3)})
-
-
-
-
-zodiac = zodiac_sign(d, m)
-
-
-if zodiac:
-    #xprint(zodiac)
-
-    ### Pandas Filtering
-    zodiac_df_western = df_western[df_western["Zodiac Sign"] == zodiac]
-    qualities_western = zodiac_df_western["Qualities"].values[0]
-    horoscope_western = zodiac_df_western["Horoscope"].values[0]
-    #print(qualities)
-
-    #print()
-    #print(horoscope)
-
-    print(f"Hey {Name}, your zodiac sign is {zodiac}. Your qualties are {qualities_western}. Your horoscope is:\n{horoscope_western}")
-
-    while True:
-
-        mayan_choice = input("Would you like to receive your Mayan Zodiac sign & horoscope? (y/n)").lower()
-
-        if mayan_choice == 'y' or mayan_choice == 'yes':
-            # mayan logic
-            zodiac_mayan = zodiac_sign_mayan(d, m)
-
-            zodiac_df_mayan = df_mayan[df_mayan["Zodiac Sign"] == zodiac_mayan]
-            qualities_mayan = zodiac_df_mayan["Qualities"].values[0]
-            horoscope_mayan = zodiac_df_mayan["Horoscope"].values[0]
-
-            print(f"{Name}, your Mayan zodiac sign is {zodiac_mayan}. Your qualties are {qualities_mayan}. Your Mayan horoscope is:\n{horoscope_mayan}")
-
-            while True:
-
-                chinese_choice = input("Would you like to receive your Chinese Zodiac sign & horoscope? (y/n)").lower()
-
-                if chinese_choice == 'y' or chinese_choice == 'yes':
-                    
-                    zodiac_df_chinese = df_chinese.iloc[zodiac_sign_chinese(y)]
-
-                    zodiac_chinese = zodiac_df_chinese["Zodiac Sign"]
-                    qualities_chinese = zodiac_df_chinese["Qualities"]
-                    horoscope_chinese = zodiac_df_chinese["Horoscope"]
-
-                    print(f"{Name}, your Chinese zodiac sign is {zodiac_chinese}. Your qualties are {qualities_chinese}. Your Chinese horoscope is:\n{horoscope_chinese}")
-
-                    break
-                
-                elif chinese_choice == 'n' or chinese_choice == 'no':
-                    break
-                else:
-                    print("Only Y or N or yes or no are accepted. Please try again!")
-                    continue
-
-            break
         
-        elif mayan_choice == 'n' or mayan_choice == "no":
-            break
+main_loop = True
+while main_loop:
 
-        else:
-            print("Only Y or N or yes or no are accepted. Please try again!")
-            continue
+    print (
+        """
+           
+                         ________    ______    _______   __       ___       ______                         
+                        |       /   /  __  \  |       \ |  |     /   \     /      |                        
+                        `---/  /   |  |  |  | |  .--.  ||  |    /  ^  \   |  ,----'                        
+                           /  /    |  |  |  | |  |  |  ||  |   /  /_\  \  |  |                             
+                          /  /----.|  `--'  | |  '--'  ||  |  /  _____  \ |  `----.                        
+                         /________| \______/  |_______/ |__| /__/     \__\ \______|                        
+                                                                                                           
+  ______     ___       __        ______  __    __   __          ___   .___________.  ______   .______      
+ /      |   /   \     |  |      /      ||  |  |  | |  |        /   \  |           | /  __  \  |   _  \     
+|  ,----'  /  ^  \    |  |     |  ,----'|  |  |  | |  |       /  ^  \ `---|  |----`|  |  |  | |  |_)  |    
+|  |      /  /_\  \   |  |     |  |     |  |  |  | |  |      /  /_\  \    |  |     |  |  |  | |      /     
+|  `----./  _____  \  |  `----.|  `----.|  `--'  | |  `----./  _____  \   |  |     |  `--'  | |  |\  \----.
+ \______/__/     \__\ |_______| \______| \______/  |_______/__/     \__\  |__|      \______/  | _| `._____|
+                                                                                                           
+                                                                               
+
+        """
+    )
+
+
+    Name=input("Enter Your Name:")
+    print ("Hello",str(Name))
+    print("This Program Will determine Your Horoscope & Zodiac Signs in the Gregorian, Mayan and Chinese calendars.")
+
+    d = int(input("Enter Day (1 through 31) ::>"))
+    m = int(input("Enter the Month (1 through 12) ::>"))
+    y = int(input("Enter the Year (From 1924 & onwards) ::>"))
+
+
+
+    df_western = pd.DataFrame({"Zodiac Sign": wks_western.row_values(1), "Qualities": wks_western.row_values(2), "Horoscope": wks_western.row_values(3)})
+
+    df_mayan = pd.DataFrame({"Zodiac Sign": wks_mayan.row_values(1), "Qualities": wks_mayan.row_values(2), "Horoscope": wks_mayan.row_values(3)})
+
+    df_chinese = pd.DataFrame({"Zodiac Sign": wks_chinese.row_values(1), "Qualities": wks_chinese.row_values(2), "Horoscope": wks_chinese.row_values(3)})
+
+
+
+
+    zodiac = zodiac_sign(d, m)
+
+
+    if zodiac:
+        
+
+        ### Pandas Filtering
+        zodiac_df_western = df_western[df_western["Zodiac Sign"] == zodiac]
+        qualities_western = zodiac_df_western["Qualities"].values[0]
+        horoscope_western = zodiac_df_western["Horoscope"].values[0]
+        
+        print(f"Hey {Name}, your zodiac sign is {zodiac}. Your qualties are {qualities_western}. Your horoscope is:\n{horoscope_western}")
+
+        while True:
+
+            mayan_choice = input("Would you like to receive your Mayan Zodiac sign & horoscope? (y/n)").lower()
+
+            if mayan_choice == 'y' or mayan_choice == 'yes':
+                # mayan logic
+                zodiac_mayan = zodiac_sign_mayan(d, m)
+
+                zodiac_df_mayan = df_mayan[df_mayan["Zodiac Sign"] == zodiac_mayan]
+                qualities_mayan = zodiac_df_mayan["Qualities"].values[0]
+                horoscope_mayan = zodiac_df_mayan["Horoscope"].values[0]
+
+                print(f"{Name}, your Mayan zodiac sign is {zodiac_mayan}. Your qualties are {qualities_mayan}. Your Mayan horoscope is:\n{horoscope_mayan}")
+
+                while True:
+
+                    chinese_choice = input("Would you like to receive your Chinese Zodiac sign & horoscope? (y/n)").lower()
+
+                    if chinese_choice == 'y' or chinese_choice == 'yes':
+                        
+                        zodiac_df_chinese = df_chinese.iloc[zodiac_sign_chinese(y)]
+
+                        zodiac_chinese = zodiac_df_chinese["Zodiac Sign"]
+                        qualities_chinese = zodiac_df_chinese["Qualities"]
+                        horoscope_chinese = zodiac_df_chinese["Horoscope"]
+
+                        print(f"{Name}, your Chinese zodiac sign is {zodiac_chinese}. Your qualties are {qualities_chinese}. Your Chinese horoscope is:\n{horoscope_chinese}")
+
+                        while True:
+
+                            continue_choice = input("Enter 'end' to end the program: ").lower()
+
+                            if continue_choice == 'end':
+                                main_loop = True
+                                break
+                            else:
+                                print("Only the word 'end' is accepted! Please try again")
+                                continue
+
+                        break
+                    
+                    elif chinese_choice == 'n' or chinese_choice == 'no':
+                        break
+                    else:
+                        print("Only Y or N or yes or no are accepted. Please try again!")
+                        continue
+
+                break
+            
+            elif mayan_choice == 'n' or mayan_choice == "no":
+                break
+
+            else:
+                print("Only Y or N or yes or no are accepted. Please try again!")
+                continue
 
 
 
